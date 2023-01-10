89a2809ffc69ebaea26a982678504d8e32130dd7
AlvesCosta <alvescosta74@gmail.com>
2023-01-10 07:38:46 -0300

    create route login

---
+++ DevBook/api/internal/controllers/login.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"api/internal/banco"
+	"api/internal/modelo"
+	"api/internal/repositorio"
+	"api/internal/respostas"
+	"api/internal/seguranca"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var usuario modelo.Usuario
+	if err = json.Unmarshal(requestBody, &usuario); err != nil {
+		respostas.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := banco.Con()
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repo := repositorio.NovoRepositoUsuarios(db)
+	usuarioSalvoNoBanco, err := repo.BuscarPorEmail(usuario.Email)
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+	if err = seguranca.VerificarSenha(usuarioSalvoNoBanco.Senha, usuario.Senha); err != nil {
+		respostas.Erro(w, http.StatusUnauthorized, err)
+		return
+	}
+	w.Write([]byte("Logado no sistema!"))
+}
