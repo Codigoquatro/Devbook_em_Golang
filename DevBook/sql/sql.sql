ae40a48b548420a2bf02b666a52c63be29d99743
Codigoquatro <codigoquatro2022@gmail.com>
2023-01-26 21:45:43 -0300

    alteração  no arquivo sql.sql

--- DevBook/sql/sql.sql
+++ DevBook/sql/sql.sql
@@ -10,3 +10,17 @@ CREATE TABLE usuarios(
     senha varchar(154) not null ,
     criadoEm timestamp default current_timestamp()
 )ENGINE=INNODB;
+
+CREATE TABLE seguidores(
+    usuario_id int not null,
+    FOREIGN KEY(usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    seguidor_id int not null,
+    FOREIGN KEY(seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    primary key(usuario_id,seguidor_id)
+)ENGINE=INNODB;
